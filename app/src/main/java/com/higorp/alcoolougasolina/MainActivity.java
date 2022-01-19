c7aee82a09bb71338fa69e389896d9ebcdb67189
Higor <cmtehigor.pe@gmail.com>
2022-01-19 14:46:13 -0300

    Correção de bugs na logica de validação dos campos e melhorias

--- app/src/main/java/com/higorp/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/higorp/alcoolougasolina/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,7 +12,6 @@ import com.google.android.material.textfield.TextInputEditText;
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText editPrecoAlcool, editPrecoGasolina;
-    private Button btnCalcular;
     private TextView txtResultado;
 
     @Override
@@ -41,9 +39,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public Boolean validarCampos (String pAlcool, String pGasolina){
-        Boolean camposValidados = true;
+        boolean camposValidados = true;
 
-        if (pAlcool == null || pAlcool.equals("") && pGasolina == null || pGasolina.equals("")){
+        if (pAlcool.equals("") && pGasolina.equals("")){
             camposValidados = false;
             Toast.makeText(getApplicationContext(), "Preencha o valor do alcool e da gasolina", Toast.LENGTH_SHORT).show();
         }else if (pAlcool == null || pAlcool.equals("")){
@@ -56,16 +54,15 @@ public class MainActivity extends AppCompatActivity {
         return camposValidados;
     }
 
-    public Double calcularMelhorPreco(String pAlcool, String pGasolina){
+    public void calcularMelhorPreco(String pAlcool, String pGasolina){
         Double valorAlcool = Double.parseDouble(pAlcool);
         Double valorGasolina = Double.parseDouble(pGasolina);
 
-        Double resultado = valorAlcool / valorGasolina;
-        if (resultado >= 0.7){
-            txtResultado.setText("Melhor usar gasolina");
-        }else {
-            txtResultado.setText("Melhor usar alcool");
+        double resultado = valorAlcool / valorGasolina;
+        if (resultado >= 0.7) {
+            txtResultado.setText(R.string.MelhorGasolina);
+        } else {
+            txtResultado.setText(R.string.MelhorAlcool);
         }
-        return resultado;
     }
 }
